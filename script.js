8c9c7a403e5e57594ff04feb1ca4466d12b714a7
mahakhalaf1 <maha344255@gmail.com>
2025-10-19 17:51:12 +0300

    Add form submission and row click alerts

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("appointmentform").onsubmit = function(event) { event.preventDefault();
+  alert("Your inquiry has been submitted!");};
+
+document.getElementsByTagName("tr")[1].onclick = function() { alert("not availlabe");};
